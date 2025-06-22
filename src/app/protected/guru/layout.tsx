f9c226518885f81eea094433b927a207d775cbff
Kuswara Senjaya <sejenakredaoficial@gmail.com>
2025-06-22 10:06:22 +0000

    2025-06-22 16:58:42.197 [error] Error: Connection error: Unknown reconne

--- src/app/protected/guru/layout.tsx
+++ src/app/protected/guru/layout.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 
-// This layout ensures that only gurus can access routes under /guru
+// This layout ensures that only gurus can access routes under /protected/guru
 // The actual UI shell is handled by (protected)/layout.tsx -> AppShell
 // This file mostly serves to group guru routes.
 

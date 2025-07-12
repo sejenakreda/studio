d1f203d2d8017967d4fd9097dff3bc6ed927cb8b
Kuswara Senjaya <sejenakredaoficial@gmail.com>
2025-07-12 12:40:04 +0000

    oke.. bagus.
    oh iya saya mengalami kendala kenapa ya PWA kita ini tidak

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,13 +23,19 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-        {/* PWA Meta Tags */}
+        
+        {/* PWA Meta Tags - Corrected & Expanded */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#3498DB" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="SiAP Smapna" />
+        
+        {/* References to actual icon files */}
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icons/icon-512x512.png" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>

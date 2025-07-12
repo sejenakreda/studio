24698fd5cd06bdb7418d2872cb6219d2da077ca4
Kuswara Senjaya <sejenakredaoficial@gmail.com>
2025-07-12 15:10:33 +0000

    untuk di laporan pelanggran, laporan kegiatan dan kemungkinan di yang la

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
